7501e9be2de5502912f0c9225f70385d11cd965b
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-07-04 22:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices=document.querySelectorAll('.price');
+	let sum=0;
+	prices.forEach((item) =>{
+		sum=sum+Number(item.innerText);
+	});
+	const table=document.getElementsByTagName("table");
+	const tr=document.createElement("tr");
+	const td=document.createElement("td");
+	td.innerText=`Total price is ${sum}`;
+	td.colspan="2";
+	td.id="ans";
+	tr.append(td);
+	table.append(tr);
+	
   
 };
 
